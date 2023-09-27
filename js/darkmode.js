5e4a61f73dd50544cdae6e9877e5e4f8449a2cff
Rick Barbosa <rick-fteam@hotmail.com>
2023-09-26 22:15:14 -0300

    my first portfolio

---
+++ js/darkmode.js
@@ -0,0 +1,7 @@
+let darkmode = document.getElementById('darkmode')
+let body = document.querySelector('body')
+
+darkmode.addEventListener('click', ()=>{
+    darkmode.classList.toggle('dark')
+    body.classList.toggle('dark')
+})
